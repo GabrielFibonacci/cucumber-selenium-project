9de87a82c339c97ffb498c8d7a4faed59b2d9575
Gabriel Gomez <gabrielfibonacci@gmail.com>
2019-05-21 15:34:53 -0500

    added the selenium web Drivers. a new file called .gitignore was created. the first background was automated

---
+++ src/main/java/com/project/selenium/stepsDefinitions/FirstTestsStepsDef.java
@@ -0,0 +1,11 @@
+package com.project.selenium.stepsDefinitions;
+
+import cucumber.api.java8.En;
+
+public class FirstTestsStepsDef implements En {
+    public FirstTestsStepsDef() {
+        Given("^I have a browser installed on the machine$", () -> {
+
+        });
+    }
+}
